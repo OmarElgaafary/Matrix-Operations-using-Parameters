1a671dbc65d8015f654a9fddafedb28927bc379f
OmarElgaafary <omarelgaafaryy@gmail.com>
2024-07-27 11:12:55 +0300

    Update parameters.cpp

--- parameters.cpp
+++ parameters.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
+void funcprint(int matrix[3][3]);
 void matrixAdd(int m1[3][3], int m2[3][3]);
 void matrixSub(int m1[3][3], int m2[3][3]);
 void matrixMul(int m1[3][3], int m2[3][3]);
-void funcprint(int array[3][3]);
 
 
 int main() {
@@ -14,13 +14,13 @@ int main() {
 	matrixMul(m1, m2);
 }
 
-void funcprint(int array[3][3]) {
+void funcprint(int matrix[3][3]) {
 
 	for (int i = 0; i < 3; i++) {
 		std::cout << "[";
 
 		for (int j = 0; j < 3; j++) {
-			std::cout << array[i][j];
+			std::cout << matrix[i][j];
 			if (j <= 1) {
 				std::cout << ", ";
 			}
@@ -62,10 +62,10 @@ void matrixMul(int m1[3][3], int m2[3][3]) {
 
 	int multiplication[3][3] = { {0, 0, 0}, {0, 0, 0}, {0, 0 ,0} };
 
-	for (int i = 0; i < 3; i++) { // i = rows
-		for (int j = 0; j < 3; j++) { // j = col
-			for (int x = 0; x < 3; x++) { // x = index of current row / col
-				multiplication[i][j] = multiplication[i][j] + m1[i][x] * m2[x][j]; // m1 rows multiplied by m2 cols
+	for (int i = 0; i < 3; i++) { 
+		for (int j = 0; j < 3; j++) { 
+			for (int x = 0; x < 3; x++) { 
+				multiplication[i][j] = multiplication[i][j] + m1[i][x] * m2[x][j]; 
 			}
 		}
 
